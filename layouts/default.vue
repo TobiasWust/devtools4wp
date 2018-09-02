bf6f4151cf493c7b383c19546a51bd4afeebb8c9
SaftpresseWust <forum@tobiaswust.de>
2018-09-02 14:57:26 +0200

    trying the background for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,6 +64,19 @@ body {
   height: 100vh;
   background-size: cover;
 }
+
+header {
+  background-image: url("~/assets/img/background.png");
+  /* background-image: url("https://res.cloudinary.com/tobiaswust/image/upload/c_scale,f_auto,q_auto:good,w_1440/v1527682422/tobiaswust.de/IMG_1080.jpg"); */
+  background-repeat: no-repeat;
+  background-position: 60% 0;
+  background-attachment: fixed;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-size: cover;
+}
+
 h1 {
   color: #111;
   text-align: center;
@@ -92,18 +105,6 @@ hr {
 .contentbg {
   transition: all 1s ease;
   background-color: #eee;
-}
-
-header {
-  background-image: url("~/assets/img/background.png");
-  /* background-image: url("https://res.cloudinary.com/tobiaswust/image/upload/c_scale,f_auto,q_auto:good,w_1440/v1527682422/tobiaswust.de/IMG_1080.jpg"); */
-  background-repeat: no-repeat;
-  background-position: 60% 0;
-  background-attachment: fixed;
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  background-size: cover;
 }
 
 /* Medium screens */
