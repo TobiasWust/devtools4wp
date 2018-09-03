3107bb435b9c4078e867a46d6cd7e3403872764f
SaftpresseWust <forum@tobiaswust.de>
2018-09-03 13:46:56 +0200

    try to work out the sticky background for mobile

--- components/eyecatcher.vue
+++ components/eyecatcher.vue
@@ -1,11 +1,9 @@
 <template>
-<div>
   <div id="eyecatcher">
     <h1><a href="/">Devtools4WP</a></h1>  
     <span class="undertext">Nutze ganz einfach die Chrome Devtools für Wordpress!</span>
     <wu_button/>
   </div>  
-</div>
 </template>
 
 <script>
@@ -55,5 +53,15 @@ h1 a {
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+
+  background-image: url("~/assets/img/background.png");
+  /* background-image: url("https://res.cloudinary.com/tobiaswust/image/upload/c_scale,f_auto,q_auto:good,w_1440/v1527682422/tobiaswust.de/IMG_1080.jpg"); */
+  background-repeat: no-repeat;
+  background-position: 60% 0;
+  background-attachment: fixed;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-size: cover;
 }
 </style>
