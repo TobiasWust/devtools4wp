2e291b633b5ab4eaa2cd7ccd6909df7da4dbbc17
SaftpresseWust <forum@tobiaswust.de>
2018-09-08 01:19:27 +0200

    show background on big screen

--- components/eyecatcher.vue
+++ components/eyecatcher.vue
@@ -58,7 +58,7 @@ h1 a {
   justify-content: space-around;
   align-items: center;
   background: none;
-  /* background-image: url("~/assets/img/background.png"); */
+  background-image: url("~/assets/img/background.png");
   /* background-image: url("https://res.cloudinary.com/tobiaswust/image/upload/c_scale,f_auto,q_auto:good,w_1440/v1527682422/tobiaswust.de/IMG_1080.jpg"); */
   background-repeat: no-repeat;
   background-position: 60% 0;
