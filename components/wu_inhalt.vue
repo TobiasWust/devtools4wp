64645613e497acfdea31a35b09c23e4c3941a5a0
SaftpresseWust <forum@tobiaswust.de>
2018-10-27 20:12:58 +0200

    freshly styled inhalt list

---
+++ components/wu_inhalt.vue
@@ -0,0 +1,100 @@
+<template>
+    <ol class="counter">
+      <li>Einleitung</li>
+      <li>Fenster Anordnen	</li>
+      <li>Html5 Seite Anlegen	</li>
+      <li>Html5 Seite Anlegen 2	</li>
+      <li>Erste Schritte In Den Devtools Und Das Boxmodel	</li>
+      <li>Mobile View	</li>
+      <li>Media Queries übersicht	</li>
+      <li>Menu Element Style	</li>
+      <li>Menu Background In Css Datei	</li>
+      <li>Konkurrierende Eigenschaften Im Computed Tab	</li>
+      <li>Eigenschaften Suchen Mit Dem Boxmodel	</li>
+      <li>Filesystem	</li>
+      <li>Cache Leeren Und Neu Laden	</li>
+      <li>Mausrad Zum Zahlen Ändern	</li>
+      <li>Text Shadow Helper	</li>
+      <li>Box Shadow Helper	</li>
+      <li>Änderungen Mit Der Konsole Finden	</li>
+      <li>Changes Tab	</li>
+      <li>Link Farbe Ändern	</li>
+      <li>Hover Im Elemtens Tab	</li>
+      <li>Filter Im Elements Tab	</li>
+      <li>Häkchen Im Elements Tab	</li>
+      <li>Unbekannte Ränder Finden Und Entfernen	</li>
+      <li>Benutzt Die Boxansicht	</li>
+      <li>Javascript In Der Konsole	</li>
+      <li>Neue Wordpress Seite Installieren	</li>
+      <li>Wordpress Customizer	</li>
+      <li>Chrome Gastmodus	</li>
+      <li>Cache Leeren Nicht Vergessen	</li>
+      <li>Inhaltsbreite Einstellen	</li>
+      <li>Farbige Kästen Zum Testen	</li>
+      <li>Devtool Zu Stylesheet	</li>
+      <li>Home Seite Anpassen	</li>
+      <li>Andere Seiten Anpassen	</li>
+      <li>Schriftart Ändern	</li>
+      <li>Site Branding Farbe	</li>
+      <li>Rahmen Suchen Und Entfernen	</li>
+      <li>Menü Einfärben	</li>
+      <li>Orientierung Im Css	</li>
+      <li>Abschluss Elements Tab	</li>
+      <li>Transition	</li>
+      <li>Transform	</li>
+      <li>Animationstab	</li>
+      <li>Snippets	</li>
+      <li>Networktab	</li>
+      <li>Performance Tab	</li>
+      <li>Application Tab	</li>
+      <li>Cookies	</li>
+      <li>Security Tab	</li>
+      <li>Audits Tab	</li>
+      <li>Slow 3G	</li>
+      <li>Andere Tabs	</li>
+      <li>Abschluss	</li>
+    </ol>
+</template>
+
+<script>
+export default {
+  name: "wu_inhalt"
+};
+</script>
+
+<style scoped>
+.counter {
+  list-style: none;
+  display: grid;
+  grid-row-gap: 1em;
+  grid-template-columns: repeat(2, 1fr);
+}
+.counter li {
+  padding-top: 0.5em;
+  padding-left: 2em;
+  margin-right: 3em;
+  position: relative;
+  counter-increment: count;
+  z-index: 90;
+  border-top: 2px solid #fc0;
+}
+.counter li::before {
+  content: counter(count);
+
+  background: #ffcc00;
+  color: #111;
+
+  font-size: 1.5em;
+  font-weight: 700;
+  font-style: italic;
+  border-radius: 50%;
+  text-align: center;
+  padding-left: 2.25%;
+  left: -1em;
+  top: -0.1em;
+  height: 1.35em;
+  width: 1.35em;
+  position: absolute;
+  z-index: -1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,8 @@
 
   <section id="inhalt">
     <h2>Inhalt:</h2>
-    <p>01 - Einleitung<br />02 - Fenster Anordnen<br />03 - Html5 Seite Anlegen<br />04 - Html5 Seite Anlegen 2<br />05 - Erste Schritte In Den Devtools Und Das Boxmodel<br />06 - Mobile View<br />07 - Media Queries &uuml;bersicht<br />08 - Menu Element Style<br />09 - Menu Background In Css Datei<br />10 - Konkurrierende Eigenschaften Im Computed Tab<br />11 - Eigenschaften Suchen Mit Dem Boxmodel<br />12 - Filesystem<br />13 - Cache Leeren Und Neu Laden<br />14 - Mausrad Zum Zahlen &Auml;ndern<br />15 - Text Shadow Helper<br />16 - Box Shadow Helper<br />17 - &Auml;nderungen Mit Der Konsole Finden<br />18 - Changes Tab<br />19 - Link Farbe &Auml;ndern<br />20 - Hover Im Elemtens Tab<br />21 - Filter Im Elements Tab<br />22 - H&auml;kchen Im Elements Tab<br />23 - Unbekannte R&auml;nder Finden Und Entfernen<br />24 - Benutzt Die Boxansicht<br />25 - Javascript In Der Konsole<br />26 - Neue Wordpress Seite Installieren<br />27 - Wordpress Customizer<br />28 - Chrome Gastmodus<br />29 - Cache Leeren Nicht Vergessen<br />30 - Inhaltsbreite Einstellen<br />31 - Farbige K&auml;sten Zum Testen<br />32 - Devtool Zu Stylesheet<br />33 - Home Seite Anpassen<br />34 - Andere Seiten Anpassen<br />35 - Schriftart &Auml;ndern<br />36 - Site Branding Farbe<br />37 - Rahmen Suchen Und Entfernen<br />38 - Men&uuml; Einf&auml;rben<br />39 - Orientierung Im Css<br />40 - Abschluss Elements Tab<br />41 - Transition<br />42 - Transform<br />43 - Animationstab<br />44 - Snippets<br />45 - Networktab<br />46 - Performance Tab<br />47 - Application Tab<br />48 - Cookies<br />49 - Security Tab<br />50 - Audits Tab<br />51 - Slow 3G<br />52 - Andere Tabs<br />53 - Abschluss</p>
+    <wu_inhalt/>
+    
   </section>
   <div class="button"><wu_button/></div>
 </div>
@@ -44,13 +45,15 @@
 
 <script>
 import wu_button from "~/components/wu_button.vue";
+import wu_inhalt from "~/components/wu_inhalt.vue";
 
 export default {
   data() {
     return {};
   },
   components: {
-    wu_button
+    wu_button,
+    wu_inhalt
   },
   head: {
     title: "Home",
@@ -139,8 +142,8 @@ export default {
 }
 
 .button {
-  position: sticky;
-  top: 0;
+  /* position: sticky;
+  top: 0; */
   margin-top: 3em;
   margin-bottom: 3em;
 }
